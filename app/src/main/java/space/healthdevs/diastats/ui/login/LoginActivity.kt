5d944366dd4486e802fbdf014689254b9aad48e8
Paul Orha <orha.paul@gmail.com>
2018-08-15 01:05:03 +0300

    Add downloadable fonts, login scrollview, dependencies
    This commit adds open sans downloadable font, adds login scrollview for horisontal compatibility and dependencies

--- app/src/main/java/space/healthdevs/diastats/ui/login/LoginActivity.kt
+++ app/src/main/java/space/healthdevs/diastats/ui/login/LoginActivity.kt
@@ -1,12 +1,20 @@
 package space.healthdevs.diastats.ui.login
 
 import android.os.Bundle
+import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_login.*
 import space.healthdevs.diastats.R
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        act_login_button_login.setOnClickListener {
+            Snackbar.make(act_login_main_layout, "Login!", Snackbar.LENGTH_SHORT).show()
+        }
+        act_login_button_register.setOnClickListener{
+            Snackbar.make(act_login_main_layout, "Register!", Snackbar.LENGTH_SHORT).show()
+        }
     }
 }
