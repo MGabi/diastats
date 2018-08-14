3696588eff75e07f61cb3a77fc8d9290e6be41c6
Paul Orha <orha.paul@gmail.com>
2018-08-15 00:03:23 +0300

    Add leakcanary
    This commit adds leakcanary

--- app/src/main/java/space/healthdevs/diastats/CustomApplication.kt
+++ app/src/main/java/space/healthdevs/diastats/CustomApplication.kt
@@ -1,13 +1,18 @@
 package space.healthdevs.diastats
 
 import android.app.Application
+import com.squareup.leakcanary.LeakCanary
 import org.koin.android.ext.android.startKoin
 import space.healthdevs.diastats.koin.AppModules
 
-class CustomApplication: Application() {
+class CustomApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            return
+        }
+        LeakCanary.install(this)
         startKoin(this, AppModules.modules)
     }
 }
